80900ff821c55b2f0db225cd9615ed3e5ab2632b
colin <colin.williams.dev@gmail.com>
2026-02-10 04:15:18 +0700

    :memo: d ts interfaces scaffold (todo)

---
+++ interface.d.ts
@@ -0,0 +1,12 @@
+// TODO: rewrite project to TypeScript and enable this file
+// @see https://www.electronjs.org/docs/latest/tutorial/context-isolation
+
+// export interface IElectronAPI {
+//     ping: () => Promise<string>;
+// }
+
+// declare global {
+//     interface Window {
+//         electronAPI: IElectronAPI;
+//     }
+// }
