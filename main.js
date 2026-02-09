917349d782cd9363b97039d44a58a20855a16758
colin <colin.williams.dev@gmail.com>
2026-02-09 15:37:58 +0700

    :sparkles: (cross platform) handles all browser windows close event on win and linux, handle activate event (mac) no browser windows open

--- main.js
+++ main.js
@@ -9,4 +9,17 @@ const createWindow = () => {
     win.loadFile("index.html");
 };
 
-app.whenReady().then(() => createWindow());
+app.whenReady().then(() => {
+    // Start and Create
+    createWindow();
+
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
