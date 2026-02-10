01eb1624be3c99c5ebe7fefbde3e27a018ae9851
colin <colin.williams.dev@gmail.com>
2026-02-11 02:16:46 +0700

    :sparkles: register IPC channels in bridge

--- bridge/preload.js
+++ bridge/preload.js
@@ -1,13 +1,21 @@
 const { contextBridge, ipcRenderer } = require("electron/renderer");
 
 contextBridge.exposeInMainWorld("api", {
+    // META
     node: () => process.versions.node,
     chrome: () => process.versions.chrome,
     electron: () => process.versions.electron,
     processContextId: () => process.contextId,
     execPath: process.execPath,
     processArchitecture: `Electron preload (bridge-context) IPC process architecture: ${process.arch}`,
+    // API
     ping: () => ipcRenderer.invoke("ping"),
+});
+
+contextBridge.exposeInMainWorld("darkMode", {
+    toggle: () => ipcRenderer.invoke("dark-mode:toggle"),
+    isDarkMode: () => ipcRenderer.invoke("dark-mode:isDarkMode"),
+    setToSystem: () => ipcRenderer.invoke("dark-mode:system"),
 });
 
 console.log(`preload ${process.contextId}`);
